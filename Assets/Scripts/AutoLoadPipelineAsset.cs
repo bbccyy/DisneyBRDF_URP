4caa73cb33acc0bd38f2944a568392c2eea4ce8b
bbccyy <yw1514@nyu.edu>
2022-05-06 01:23:25 +0800

    update

---
+++ Assets/Scripts/AutoLoadPipelineAsset.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class AutoLoadPipelineAsset : MonoBehaviour
+{
+    public UniversalRenderPipelineAsset pipelineAsset;
+
+    private void OnEnable()
+    {
+        if (pipelineAsset != null)
+        {
+            GraphicsSettings.renderPipelineAsset = pipelineAsset;
+        }
+    }
+}
